d2a2891191936d26e25f06b51b6aa3fccbf9e89d
Francisco Martinez <panchetz7@gmail.com>
2022-01-30 23:39:37 -0800

    set up lists get and post routes

--- routes/lists.js
+++ routes/lists.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const List = require('../models/List');
 
 // @route   GET api/lists
 // @desc    Get all users lists
 // @access    Private
-router.get('/', (req, res) => {
-  res.send('Get all lists');
+router.get('/', auth, async (req, res) => {
+  try {
+    const lists = await List.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(lists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/lists
 // @desc    Add new list
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add list');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email } = req.body;
+
+    try {
+      const newList = new List({
+        name,
+        email,
+        user: req.user.id,
+      });
+
+      const list = await newList.save();
+
+      res.json(list);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   PUT api/lists/:id
 // @desc    Update list
