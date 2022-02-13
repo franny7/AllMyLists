1b61d5f1234d4d559392e656beefe1cc50c3534b
Francisco Martinez <panchetz7@gmail.com>
2022-02-13 14:55:33 -0800

    create login&register forms/routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,32 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import ListState from './context/list/ListState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <ListState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/about' element={<About />} />
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ListState>
+    <AuthState>
+      <ListState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/about' element={<About />} />
+                <Route exact path='/register' element={<Register />} />
+                <Route exact path='/login' element={<Login />} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </ListState>
+    </AuthState>
   );
 };
 
--- client/src/components/lists/Lists.js
+++ client/src/components/lists/Lists.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ListItem from './ListItem';
 import ListContext from '../../context/list/listContext';
 
@@ -8,14 +9,24 @@ const Lists = () => {
   const { lists, filtered } = listContext;
 
   if (lists.length === 0) {
-    return <h4>Please add a list item</h4>;
+    return <h4>Add a list item</h4>;
   }
 
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((list) => <ListItem key={list.id} list={list} />)
-        : lists.map((list) => <ListItem key={list.id} list={list} />)}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((list) => (
+              <CSSTransition key={list.id} timeout={500} classNames='item'>
+                <ListItem list={list} />
+              </CSSTransition>
+            ))
+          : lists.map((list) => (
+              <CSSTransition key={list.id} timeout={500} classNames='item'>
+                <ListItem list={list} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
